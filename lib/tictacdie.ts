1022002b08e63475ffdc0efe6405c7532cd20e30
nymbus <nymbus@YoungNewSystem.YoungNewSystem>
2026-06-13 19:26:47 +0200

    lib game part1

--- lib/tictacdie.ts
+++ lib/tictacdie.ts
@@ -1,11 +1,11 @@
 export type Empty = "";
 export type Symbol = "X" | "O";
 
-export type Nomad = {dirx: number, diry: number, old_value: Empty | Symbol};
-export type Immunity = {cooldown: number};
-export type Trap = {newx: number, newy: number};
-export type Virus = {content: Symbol};
-export type TTT = {}; // TODO
+export type Nomad = {kind: "nomad", cooldown: number, dirx: number, diry: number, content: Symbol, old_content: Cell};
+export type Immunity = {kind: "immunity", cooldown: number, content: Cell};
+export type Trap = {kind: "trap", newx: number, newy: number};
+export type Virus = {kind: "virus", content: Empty | Symbol};
+export type TTT = {kind: "ttt"}; // TODO
 
 export type Cell = Empty | Symbol | Nomad | Immunity | Trap | Virus | TTT;
 	
@@ -27,17 +27,94 @@ export class Game {
 		this.p1 = p1; this.p2 = p2;
 	}; // constructor
 
-	tick() {}; // tick
+	tick() {
+		// console.log("START", this.board);
+		for (let j=0; j < this.board.length; j++) {
+			for (let i=0; i < this.board[0].length; i++) {
+				let cell = this.board[j][i];
+
+				if (typeof cell === "object") {
+					if (cell.kind === "nomad") {
+						if (cell.cooldown >= 0) cell.cooldown--;
+						if (cell.cooldown < 0) {
+							this.board[j][i] = cell.old_content;
+
+							const newx = i + cell.dirx, newy = j + cell.diry;
+							if ((newx >= 0 && newx < this.board[0].length) && (newy >= 0 && newy < this.board.length)) {
+								cell.old_content = this.board[newy][newx];
+								this.board[newy][newx] = cell;
+							} 
+							if (cell.dirx > 0 || cell.diry > 0) cell.cooldown = 1;
+						}
+
+					} else if (cell.kind === "immunity") {
+						cell.cooldown--;
+						if (cell.cooldown < 0) this.board[j][i] = cell.content;
+
+					} else if (cell.kind === "trap") {
+					} else if (cell.kind === "virus") {
+						let countX = 0, countO = 0;
+
+						const cross = [[-1, 0], [1, 0], [-1, -1], [0, -1], [1, -1], [-1, 1], [0, 1], [1, 1]];
+						for (const [dy, dx] of cross) {
+							if (i + dx >= 0 && i + dx < this.board[0].length
+							    	&& j + dy >= 0 && j + dy < this.board.length) {
+								if (this.board[j + dy][i + dx] === "X") countX++;
+								if (this.board[j + dy][i + dx] === "O") countO++;
+							}
+						}
+						if (countX > countO) {
+							cell.content = "X";
+						} else if (countX < countO) {
+							cell.content = "O";
+						} else { cell.content = ""; }
+
+					} else if (cell.kind === "ttt") {
+					}
+				}
+
+			}
+		}
+		// console.log("END", this.board);
+	}; // tick	
+
 	isGameOver(): number { return -1; }; // isGameOver
 
 	applyInvert(row: number, column: number) {}; // applyInvert
-	applyResize(top: boolean, bottom: boolean, left: boolean, right: boolean) {}; // applyResize
+	applyResize(top: boolean, bottom: boolean) {}; // applyResize
 	applyBomb(x: number, y: number) {}; // applyBomb
 
-	placeSymbol(x: number, y: number, s: Symbol) {}; // placeSymbol
-	placeNomad(x: number, y: number, dirx: number, diry: number) {}; // placeNomad
-	placeImmunity(x: number, y: number) {}; // placeImmunity
-	placeVirus(x: number, y: number) {}; // placeVirus
-	placeTrap(x: number, y: number, ax: number, ay: number) {}; // placeTrap
+	placeSymbol(x: number, y: number, s: Symbol): boolean {
+		if (this.board[y][x] !== "") return false;
+		this.board[y][x] = s;
+		return true;
+	}; // placeSymbol
+
+	placeNomad(x: number, y: number, dirx: number, diry: number, s: Symbol): boolean {
+		if (x !== 0 && x !== this.board[0].length-1 && y !== 0 && y !== this.board.length-1) return false;
+		if ((diry === 0 && dirx !== -1 && dirx !== 1) || (dirx === 0 && diry !== -1 && diry !== 1)) return false;
+
+		const content = this.board[y][x];
+		this.board[y][x] = {kind: "nomad", cooldown: 1, dirx: dirx, diry: diry, content: s, old_content: content};
+		return true;
+	}; // placeNomad
+
+	placeImmunity(x: number, y: number): boolean {
+		if (x < 0 || x > this.board[0].length-1) return false;
+		if (y < 0 || y > this.board.length-1) return false;
+
+		const content = this.board[y][x];
+		this.board[y][x] = {kind: "immunity", cooldown: 2, content: content};
+		return true;
+	}; // placeImmunity
+
+	placeVirus(x: number, y: number): boolean {
+		if (x < 0 || x > this.board[0].length-1) return false;
+		if (y < 0 || y > this.board.length-1) return false;
+		this.board[y][x] = {kind: "virus", content: ""};
+		return true;
+	}; // placeVirus
+	
+	placeTrap(x: number, y: number, ax: number, ay: number): boolean { return true; }; // placeTrap
 	placeTTT(x: number, y: number) {}; // placeTTT
 }; // class Game
--- lib/tictacdie_test.ts
+++ lib/tictacdie_test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "jsr:@std/assert";
 import * as ttd from "./tictacdie.ts";
 
-Deno.test("init game", () => {
+Deno.test("init Game", () => {
 	const g = new ttd.Game({name:"player1"}, {name:"player2"});	
 
 	assertEquals(g.board.length, 3);
@@ -15,6 +15,75 @@ Deno.test("init game", () => {
 		}
 	}
 });
+
+Deno.test("placeSymbol", () => {
+	const g = new ttd.Game({name:"player1"}, {name:"player2"});	
+	g.placeSymbol(0, 0, "X");
+	assertEquals(g.board[0][0], "X");
+
+	g.placeSymbol(2, 1, "O");
+	assertEquals(g.board[1][2], "O");
+});
+
+Deno.test("placeNomad", () => {
+	const g = new ttd.Game({name:"player1"}, {name:"player2"});	
+
+	g.placeNomad(0, 0, 1, 0, "X");
+	g.tick();
+	assertEquals(g.board[0][0], {kind: "nomad", cooldown: 0, dirx:1, diry:0, content: "X", old_content: ""});
+	g.tick();
+	assertEquals(g.board[0][1], {kind: "nomad", cooldown: 0, dirx:1, diry:0, content: "X", old_content: ""});
+	g.tick();
+	g.tick();
+	assertEquals(g.board[0][2], "");
+
+	g.placeSymbol(1, 1, "O");
+	g.placeNomad(1, 2, 0, -1, "X");
+	g.tick();
+	assertEquals(g.board[2][1], {kind: "nomad", cooldown: 0, dirx:0, diry:-1, content:"X", old_content: ""});
+	g.tick();
+	assertEquals(g.board[1][1], {kind: "nomad", cooldown: -1, dirx:0, diry:-1, content:"X", old_content: "O"});
+	g.tick();
+	g.tick();
+	assertEquals(g.board[0][1], "");
+});
+
+Deno.test("placeImmunity", () => {
+	const g = new ttd.Game({name:"player1"}, {name:"player2"});	
+	
+	g.placeImmunity(0, 0);
+	assertEquals(g.board[0][0], {kind: "immunity", cooldown:2, content: ""});
+	g.tick();
+	assertEquals(g.board[0][0], {kind: "immunity", cooldown:1, content: ""});
+	g.tick();
+	g.tick();
+	assertEquals(g.board[0][0], "");
+});
+
+Deno.test("placeVirus", () => {
+	const g = new ttd.Game({name:"player1"}, {name:"player2"});	
+
+	g.placeVirus(0, 0);
+	assertEquals(g.board[0][0], {kind: "virus", content: ""});
+
+	g.placeSymbol(1, 0, "X");
+	g.tick();
+	assertEquals(g.board[0][0], {kind: "virus", content: "X"});
+
+	g.placeSymbol(0, 1, "O");
+	g.tick();
+	assertEquals(g.board[0][0], {kind: "virus", content: ""});
+
+	g.placeSymbol(1, 1, "O");
+	g.tick();
+	assertEquals(g.board[0][0], {kind: "virus", content: "O"});
+});
+
+Deno.test("placeTrap", () => {
+});
+
+Deno.test("placeTTT", () => {});
+
 /*
 	tick() {}; // tick
 	isGameOver(): number {}; // isGameOver
